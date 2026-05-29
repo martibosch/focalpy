dee7ccb197bf33745035a4fa770f4875a24e4df8
Martí Bosch <marti.bosch@protonmail.com>
2026-05-29 11:41:57 +0200

    feat: as_coord_arr geoseries to coord array utility

--- focalpy/utils.py
+++ focalpy/utils.py
@@ -3,6 +3,8 @@
 import os
 from collections.abc import Mapping
 
+import numpy as np
+
 ########################################################################################
 # type annotations
 # type hint for path-like objects
@@ -11,3 +13,23 @@ PathType = str | os.PathLike
 KwargsType = Mapping | None
 # type hint for fillna arguments
 FillnaType = float | Mapping | bool | None
+
+
+########################################################################################
+# geo utils
+def as_coord_arr(site_gser):
+    """Convert a GeoSeries of point geometries to a coordinate array.
+
+    Parameters
+    ----------
+    site_gser : geopandas.GeoSeries or array-like of shape (n, 2)
+        Site locations. If a :class:`~geopandas.GeoSeries`, x/y
+        coordinates are extracted from the point geometries.
+
+    Returns
+    -------
+    numpy.ndarray of shape (n, 2)
+    """
+    if hasattr(site_gser, "x") and hasattr(site_gser, "y"):
+        return np.column_stack([site_gser.x, site_gser.y])
+    return np.asarray(site_gser)
